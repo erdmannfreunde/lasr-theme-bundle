b770d935bd1c18edf9396b35e3f8e4713659d408
Dennis Erdmann <dennis@erdmann-freunde.de>
2026-03-02 16:05:00 +0100

    Füge Plugin- und Hauptbundle-Klassen für das LasrThemeBundle hinzu und aktualisiere die Autoload-Konfiguration in composer.json

---
+++ src/LasrThemeBundle.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace ErdmannFreunde\LasrThemeBundle;
+
+use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
+
+class LasrThemeBundle extends AbstractBundle
+{
+}
